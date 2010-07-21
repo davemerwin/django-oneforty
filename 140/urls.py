ccb227b166741a02293e96baf8f0e03d39312fde
Dave Merwin <dmerwin@orcasinc.com>
2010-07-21 16:00:52 -0700

    updating what the URLs will be

--- 140/urls.py
+++ 140/urls.py
@@ -1,5 +1,15 @@
 from django.conf.urls.defaults import *
+from 140.views import *
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    
+    # Get ALL the updates
+    url(r'^/', '140.views.list_all', name='all_updates'),
+    # Get all the updates for a username
+    url(r'^(?P<username>[\w-]+)/$', '140.views.user_list', name='username_updates'),
+    # Get a single update
+    url(r'^(?P<username>[\w-]+)/(?P<id>\d+)/$', '140.views.update_detail', name='update_detail'),
+    # Create an update
+    url(r'^update/', '140.views.update', name='140_update'),
 )
