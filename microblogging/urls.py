84678d7e02e72f55af53ce2921e68df3f1810db2
Dave Merwin <dave@davemerwin.com>
2010-07-20 20:54:24 -0700

    getting started with VERY basic sqlite - will be removed.

--- microblogging/urls.py
+++ microblogging/urls.py
@@ -5,13 +5,5 @@ from django.conf.urls.defaults import *
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^microblogging/', include('microblogging.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    
 )
