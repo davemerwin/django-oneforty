430ae75886afb57346e74f0bcb248bb7bc949fbb
Dave Merwin <dmerwin@orcasinc.com>
2010-07-21 16:59:16 -0700

    adding some templates and starting to flesh out views

--- oneforty/views.py
+++ oneforty/views.py
@@ -21,11 +21,25 @@ def oneforty_list_all(request):
     """
     Get all of the updates
     """
+    return object_list(
+    request,
+    Update.objects.all(),
+    paginate_by=25,
+    template_name='oneforty/oneforty_list_all.html',
+    allow_empty=True
+    )
     
 def oneforty_user_list(request, username):
     """
     Get all the updates for a user
     """
+    return object_list(
+    request,
+    Update.objects.filter(username=user.username),
+    paginate_by=25,
+    template_name='oneforty/oneforty_username_list_all.html',
+    allow_empty=True
+    )
     
 def oneforty_update_detail(request, username, id):
     """
