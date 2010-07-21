65f1c2ae1a9e4e99822c5ad74d2bfec2ce8d95fb
Dave Merwin <dmerwin@orcasinc.com>
2010-07-21 16:16:56 -0700

    create basic views structure

--- 140/views.py
+++ 140/views.py
@@ -1,1 +1,28 @@
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render_to_response
+from django.template import RequestContext
+
+from 140.models import Update
+
+@login_required
+def update(request, username):
+    """
+    Create the actual update
+    """
+    
+def list_all(request):
+    """
+    Get all of the updates
+    """
+    
+def user_list(request, username):
+    """
+    Get all the updates for a user
+    """
+    
+def update_detail(request, username, id):
+    """
+    The permalink for an update
+    """
